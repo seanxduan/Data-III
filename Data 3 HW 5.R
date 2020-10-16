04e1fcc5922536bf1667dfb166aae088eda9d848
sean x duan <seanxduan@gmail.com>
2020-10-16 15:52:13 -0500

    finished more of hw5, stuck on how to optimze mars, how to use boosting on fish

--- Data 3 HW 5.R
+++ Data 3 HW 5.R
@@ -3,6 +3,7 @@ library(tree)
 library(ISLR)
 library(randomForest)
 library(gbm)
+library(earth)
 attach(Carseats)
 data(Carseats)
 set.seed(1)
@@ -116,8 +117,7 @@ varImpPlot(carseat_rf[[which.min(test_mse)]])
 #used tutorial from
 #http://uc-r.github.io/mars
 
-install.packages("earth")
-library(earth)
+
 carseat_mars<-earth(Sales~.,data=Carseats, subset = train)
 summary(carseat_mars)
 print(carseat_mars)
@@ -130,5 +130,107 @@ plot(carseat_mars, which=1)
 yhat=predict(carseat_mars,newdata=Carseats[-train,])
 mean((yhat -Carseats_test)^2)
 #WOWOWOW lowest test MSE yet!
-#2
 
+#2
+#load up our fish data and prep it
+load("fish_data3.RData")
+fish<-fish_data3
+fish$LSH7class = droplevels(fish$LSH7class)
+
+#split into test/training
+set.seed(1)
+training.set=sample(1:nrow(fish),400)
+fish.test=fish[-training.set,]
+LSH7class.test=fish.test[,12]
+#goal - lowest classif error (use pred to find that out)
+
+#find best CART bagging
+fish_bag=randomForest(LSH7class~.,data=fish , subset=training.set ,mtry=11,importance =TRUE)
+fish_bag
+#error rate 4.75
+#checking test MSE
+yhat.bag = predict (fish_bag , newdata=fish[-training.set ,])
+plot(yhat.bag , LSH7class.test)
+#importance of variables
+importance(fish_bag)
+varImpPlot(fish_bag)
+#secchi lake mean, area hectacres most important, hectacres are good for accuracy, not for node purity!
+
+table(yhat.bag,LSH7class.test)
+1-mean(yhat.bag==LSH7class.test)
+#3% class error!
+
+
+#Random Forest
+#we start w/ a loop to find the best m value!
+mvec<-seq(from=1, to=11)
+yhat.bag<-list(NA)
+fish_rf<-list(NA)
+class_err<-list(NA)
+
+for(i in 1:11){
+  fish_rf[[i]]=randomForest(LSH7class~.,data=fish , subset=training.set ,mtry=mvec[i],importance =TRUE)
+  yhat.bag[[i]]<-predict(fish_rf[[i]] , newdata=fish[-training.set,])
+  class_err[[i]]<-(1-mean(yhat.bag[[i]]==LSH7class.test))
+}
+class_err
+which.min(class_err)
+#setting m to 2 gets us the lowest class error
+#1.932% error, good!
+#see if ntrees works
+testn<-seq(from=300, to=3000, length.out = 10)
+
+for(i in 1:10){
+  fish_rf[[i]]=randomForest(LSH7class~.,data=fish , subset=training.set ,mtry=2, ntree=testn[[i]], importance =TRUE)
+  yhat.bag[[i]]<-predict(fish_rf[[i]] , newdata=fish[-training.set,])
+  class_err[[i]]<-(1-mean(yhat.bag[[i]]==LSH7class.test))
+}
+class_err
+which.min(class_err)
+#seems like length doesn't matter that much, other than >300 and <3000 affectin class error
+
+importance(fish_rf[[which.min(class_err)]])
+varImpPlot(fish_rf[[which.min(class_err)]])
+#RF consideres secchi lake mean, area hectacres, mean GDD, max depth to be good for accuracy
+#Considers secchi lake mean, mean gdd and hectacres for node purity
+
+#mars
+fish_mars<-earth(LSH7class~.,data=fish, subset = training.set)
+summary(fish_mars)
+print(fish_mars)
+
+#model selection code for mars
+plot(fish_mars, which=1)
+#mars models scale invariant
+
+#test err code
+yhat=predict(fish_mars,newdata=fish[-training.set,])
+mars.pred=rep("LargeEutrophic" ,207)
+mars.pred[yhat >.5]="LargeMesotrophic"
+table(mars.pred, LSH7class.test)
+1-mean(mars.pred==LSH7class.test)
+#3.86% error, not better than others
+#var importance code
+evimp(fish_mars)
+#plottin it too
+
+#doesnt work atm but lets screw it out
+library(vip)
+p1 <- vip(fish_mars, num_features = 11, bar = FALSE, value = "gcv") + ggtitle("GCV")
+p2 <- vip(fish_mars, num_features = 11, bar = FALSE, value = "rss") + ggtitle("RSS")
+gridExtra::grid.arrange(p1, p2, ncol = 2)
+##
+
+#best CART boosting
+yhat.boost<-list(NA)
+fish_boost<-list(NA)
+
+testn<-seq(from=300, to=3000, length.out = 10)
+for (i in 1:10){
+  fish_boost[[i]]=gbm(LSH7class~.,data=fish[training.set,], distribution="gaussian",n.trees=testn[[i]], interaction.depth=1)
+  yhat.boost[[i]]=predict(fish_boost[[i]] ,newdata =fish[-training.set ,],n.trees=testn[[i]])
+  class_err[[i]]<-(1-mean(yhat.boost[[i]]==LSH7class.test))
+}
+class_err
+which.min(class_err)
+#can't get boosting to work?
