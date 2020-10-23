eed4a7c45ff404e36209b6be4425e1b0ca4962d6
sean x duan <seanxduan@gmail.com>
2020-10-23 14:08:59 -0500

    one moreb onus bit

--- house-prices-advanced-regression-techniques/Kaggle Competition 1.R
+++ house-prices-advanced-regression-techniques/Kaggle Competition 1.R
@@ -40,3 +40,33 @@ vif(lm2)
 #terms to look @ kicking out
 #sale type, sale condition, gr liv area, foundation, exterior 1st, exter qual, bldgtype, housestyle, neighborhood, 
 #mssubclass, mszoning
+
+
+
+##lets try a fresh stab using other methods... perhaps regression tree methods?
+library(tree)
+library(ISLR)
+library(MASS)
+#ensure that we have factors instead of characters
+library(tidyverse)
+houses_clean <- houses_clean %>% mutate_if(is.character,as.factor)
+#perhaps push this code up earlier so we dont have 2 do it here?
+tr1<-tree(SalePrice~., data=houses_clean)
+summary(tr1)
+#only 5 variables used here, overall qual, neighborhood, Grlivarea, total basementsf, year remmodel add
+plot(tr1)
+text(tr1, pretty=0)
+#try to prune or basic tree
+cv_tr1<-cv.tree(tr1)
+plot(cv_tr1$size ,cv_tr1$dev ,type="b")
+#looks like our largest tree is the best, but perhaps a 5 node tree is comparable
+
+## bagging?
+library(randomForest)
+tr2<-randomForest(SalePrice~., data=houses_clean, mtry = 54, importance = TRUE)
+tr2
+summary(tr2)
+importance(tr2)
+varImpPlot(tr2)
+#overall qual best for node purity
+#mse red is gr liv area, neighborhood, overall qual, totalbsmt SF
